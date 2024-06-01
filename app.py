64d528915ed881753a1071373bb0cdb6273d9962
Nidhi-S-Rao <49355817+Nidhi-S-Rao@users.noreply.github.com>
2024-06-01 19:11:26 +0530

    Update app.py

--- app.py
+++ app.py
@@ -103,4 +103,4 @@ def scrape_data_CSV():
     
 if __name__ == '__main__':
     #app.run(debug=True)
-    app.run(dubug=False,host='0.0.0.0',port=5000)
+    app.run(debug=True,host='0.0.0.0',port=5000)
