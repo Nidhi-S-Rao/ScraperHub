ab5687cc057edfb81ec44f69c3bccbda7fffbee5
Nidhi-S-Rao <49355817+Nidhi-S-Rao@users.noreply.github.com>
2024-06-03 13:11:27 +0530

    Update app.py

--- app.py
+++ app.py
@@ -96,14 +96,14 @@ def scrape_data_CSV():
             w.writeheader()
             for element in tags_list:
                 w.writerow(element)
-        return send_file(filename, as_attachment=True, attachment_filename=filename, mimetype='text/csv')
+        return send_file(filename, as_attachment=True, download_name=filename, mimetype='text/csv')
     #change attachment_filename to download_name if any error 
     
     elif value == 'json':
         filename = 'download.json'
         with open(filename, 'w', encoding='utf-8') as f:
             json.dump(tags_list, f, indent=4, ensure_ascii=False)
-        return send_file(filename, as_attachment=True, attachment_filename=filename, mimetype='application/json')
+        return send_file(filename, as_attachment=True, download_name=filename, mimetype='application/json')
     #change attachment_filename to download_name if any error 
 
     
@@ -119,7 +119,7 @@ def scrape_data_CSV():
         output = BytesIO()
         workbook.save(output)
         output.seek(0)
-        return send_file(output, as_attachment=True, attachment_filename=filename, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
+        return send_file(output, as_attachment=True, download_name=filename, mimetype='application/vnd.openxmlformats-officedocument.spreadsheetml.sheet')
         #change attachment_filename to download_name if any error 
 
     elif (value=='visualization'):
